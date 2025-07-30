2e36d1a282bc7fd71578f0d0717b00e460b51529
Ellen Avrumova <e.avrumova@gmail.com>
2025-07-30 15:21:15 -0400

    Initial commit

---
+++ src/main/java/com/myapp/trade/TradeReconciliationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeReconciliationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeReconciliationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myapp/trade/TradeReconciliationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.trade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeReconciliationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
